75c620b8a0082d9ef0a60fe784b76383f63b1684
Naphorn <naphorn.chok@gmail.com>
2017-10-20 23:38:09 +0700

    From Argument with Wide

---
+++ Part07/Part07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 123456789;
+            Console.WriteLine("{0, 20:E}", 1);
+            Console.WriteLine("{0, 20:F}", 1);
+            Console.WriteLine("{0, 20:G}", 1);
+            Console.WriteLine("{0, 20:N}", 1);
+            Console.WriteLine("{0, 20:P}", 1);
+            Console.WriteLine("{0, 20:X}", 1);
+        }
+    }
+}
