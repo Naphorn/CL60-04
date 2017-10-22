14b5c89248f06dadba7d2c39f7561ce8229c1ade
Naphorn <naphorn.chok@gmail.com>
2017-10-23 02:24:49 +0700

    Number 6

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -6,7 +6,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("|{0, 10}|", 999);
+            Console.WriteLine("|{0,-10}|", 000);
         }
     }
 }
