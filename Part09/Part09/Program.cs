9edeb67ad26fd2bd679a135dd3da613b27c65dfb
Naphorn <naphorn.chok@gmail.com>
2017-10-23 02:49:52 +0700

    input by keyboard

---
+++ Part09/Part09/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.Write("Press a key followed by ENTER:");
+            ch = (char)Console.Read();  // get a char
+            Console.WriteLine("Your key is: " + ch);
+        }
+    }
+}
