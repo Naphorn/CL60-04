148f520b18bf74b1b96705e78a718ad1753512cc
Naphorn <naphorn.chok@gmail.com>
2017-10-23 02:55:09 +0700

    input String by keyboard

---
+++ Part10/Part10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.Write("Enter some characters.");
+            str = Console.Read();
+            Console.WriteLine("You entered: " + str);
+        }
+    }
+}
