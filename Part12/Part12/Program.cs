b20f20381796bcdb1dfdf7d3595a9ea8f13c720c
Naphorn <naphorn.chok@gmail.com>
2017-10-23 18:34:47 +0700

    Test Name ID

---
+++ Part12/Part12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.Write("Please enter Name : ");
+                string fname = Console.ReadLine();
+                Console.Write("Please enter Lastname : ");
+                string lname = Console.ReadLine();
+                Console.Write("Please enter ID : ");
+                int val1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter Grade : ");
+                string val2 = (Console.ReadLine());
+                
+                Console.WriteLine("Name : " +fname);
+                Console.WriteLine("Lastname : " + lname);
+                Console.WriteLine("ID : " + val1);
+                Console.WriteLine("Grade : " + val2)ว
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error : " + e.ToString());
+            }
+        }
+    }
+}
