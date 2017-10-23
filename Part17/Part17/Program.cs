b22474f4453fec7131754fcd531eca3661e4dbf3
Naphorn <naphorn.chok@gmail.com>
2017-10-23 20:19:19 +0700

    Math

---
+++ Part17/Part17/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class intergerTest
+{
+    static void Main(string[] args)
+    {
+        int a, b, c, d, e, f;
+        a = 1;
+        b = a + 6;
+        c = b - 3;
+        d = c * 2;
+        e = d / 2;
+        f = e % 2;
+    }
+}
