28e92c227a6231ad40796f5e0272abbd3579166d
Naphorn <naphorn.chok@gmail.com>
2017-10-23 16:39:33 +0700

    Input Number

---
+++ Part11/Part11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.Write("Please enter value 1 : ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(val1);
+        }
+    }
+}
