9e86aa7655429e5d4ab6f514c935b16af1f2f6dc
Naphorn <naphorn.chok@gmail.com>
2017-10-23 19:12:43 +0700

    Data type by Char

--- Part13/Part13/Program.cs
+++ Part13/Part13/Program.cs
@@ -7,8 +7,8 @@ namespace variableProperties
         {
             Console.WriteLine("Data type : char");
             Console.WriteLine("Size :" + sizeof(char));
-            Console.WriteLine("Minimum Value :" + char.MinValue);
-            Console.WriteLine("Maximum Value :" + char.MaxValue);
+            Console.WriteLine("Minimum Value :" + (int)char.MinValue);
+            Console.WriteLine("Maximum Value :" + (int)char.MaxValue);
         }
     }
 }
