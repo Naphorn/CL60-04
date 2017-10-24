61e1bfd02aa13355ce6a8d6da7e72d8fcf8c8a52
Naphorn <naphorn.chok@gmail.com>
2017-10-24 16:23:01 +0700

    Test 2 Math.Cos

--- Part19/Part19/Program.cs
+++ Part19/Part19/Program.cs
@@ -5,7 +5,7 @@ public class MathTest
     {
         for (float i = 0; i < Math.PI * 2.0F; i += 0.3F)
         {
-            Console.WriteLine("The sine of {0,10:F} = {1,-10:F6}" +
+            Console.WriteLine("The cos of {0,10:F} = {1,-10:F6}" +
             spaces(Math.Cos(i)) + "*", i, Math.Cos(i));
         }
     }
